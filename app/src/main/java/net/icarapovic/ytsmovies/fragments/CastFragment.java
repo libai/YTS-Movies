44a452a28ba1d76d3901b7b2e8006805c1a01bc8
icarapovic <icarapovi@gmail.com>
2015-05-30 20:36:02 +0200

    Cast with circular images

--- app/src/main/java/net/icarapovic/ytsmovies/fragments/CastFragment.java
+++ app/src/main/java/net/icarapovic/ytsmovies/fragments/CastFragment.java
@@ -17,6 +17,8 @@ import net.icarapovic.ytsmovies.models.Actor;
 import net.icarapovic.ytsmovies.models.Director;
 import net.icarapovic.ytsmovies.models.Movie;
 
+import org.w3c.dom.Text;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CastFragment extends Fragment{
@@ -44,19 +46,23 @@ public class CastFragment extends Fragment{
         Context c = getActivity().getApplicationContext();
 
         ImageView actImg1, actImg2, actImg3, actImg4;
-        TextView dirName, actName1, actName2, actName3, actName4;
+        TextView dirName, actName1, actName2, actName3, actName4, char1, char2, char3, char4;
         CircleImageView dirImg;
 
         dirImg = (CircleImageView) v.findViewById(R.id.director_img);
         dirName = (TextView) v.findViewById(R.id.director);
-        actImg1 = (ImageView) v.findViewById(R.id.actorImg1);
-        actImg2 = (ImageView) v.findViewById(R.id.actorImg2);
-        actImg3 = (ImageView) v.findViewById(R.id.actorImg3);
-        actImg4 = (ImageView) v.findViewById(R.id.actorImg4);
-        actName1 = (TextView) v.findViewById(R.id.actorName1);
-        actName2 = (TextView) v.findViewById(R.id.actorName2);
-        actName3 = (TextView) v.findViewById(R.id.actorName3);
-        actName4 = (TextView) v.findViewById(R.id.actorName4);
+        actImg1 = (ImageView) v.findViewById(R.id.actImg1);
+        actImg2 = (ImageView) v.findViewById(R.id.actImg2);
+        actImg3 = (ImageView) v.findViewById(R.id.actImg3);
+        actImg4 = (ImageView) v.findViewById(R.id.actImg4);
+        actName1 = (TextView) v.findViewById(R.id.actName1);
+        actName2 = (TextView) v.findViewById(R.id.actName2);
+        actName3 = (TextView) v.findViewById(R.id.actName3);
+        actName4 = (TextView) v.findViewById(R.id.actName4);
+        char1 = (TextView) v.findViewById(R.id.char1);
+        char2 = (TextView) v.findViewById(R.id.char2);
+        char3 = (TextView) v.findViewById(R.id.char3);
+        char4 = (TextView) v.findViewById(R.id.char4);
 
         Picasso.with(c).load(args.getString(Director.MEDIUM_IMAGE)).into(dirImg);
         Picasso.with(c).load(args.getStringArray(Actor.MEDIUM_IMG)[0]).into(actImg1);
@@ -65,10 +71,14 @@ public class CastFragment extends Fragment{
         Picasso.with(c).load(args.getStringArray(Actor.MEDIUM_IMG)[3]).into(actImg4);
 
         dirName.setText(args.getString(Director.NAME));
-        actName1.setText(args.getStringArray(Actor.NAME)[0] + " \nas \n" + args.getStringArray(Movie.CHARACTERS)[0]);
-        actName2.setText(args.getStringArray(Actor.NAME)[1] + " \nas \n" + args.getStringArray(Movie.CHARACTERS)[1]);
-        actName3.setText(args.getStringArray(Actor.NAME)[2] + " \nas \n" + args.getStringArray(Movie.CHARACTERS)[2]);
-        actName4.setText(args.getStringArray(Actor.NAME)[3] + " \nas \n" + args.getStringArray(Movie.CHARACTERS)[3]);
+        actName1.setText(args.getStringArray(Actor.NAME)[0] + " as");
+        actName2.setText(args.getStringArray(Actor.NAME)[1] + " as");
+        actName3.setText(args.getStringArray(Actor.NAME)[2] + " as");
+        actName4.setText(args.getStringArray(Actor.NAME)[3] + " as");
+        char1.setText(args.getStringArray(Movie.CHARACTERS)[0]);
+        char2.setText(args.getStringArray(Movie.CHARACTERS)[1]);
+        char3.setText(args.getStringArray(Movie.CHARACTERS)[2]);
+        char4.setText(args.getStringArray(Movie.CHARACTERS)[3]);
 
         return v;
     }
